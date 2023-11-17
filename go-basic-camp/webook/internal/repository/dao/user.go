1434cc478adbd10c8f3c1a2a95120eca11e740e4
upupnoah <upupqi.cs@gmail.com>
2023-11-17 20:24:32 +0800

    feat(webook): add the session and jwt

--- go-basic-camp/webook/internal/repository/dao/user.go
+++ go-basic-camp/webook/internal/repository/dao/user.go
@@ -2,9 +2,18 @@ package dao
 
 import (
 	"context"
+	"errors"
 	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/upupnoah/go-basic-future/go-basic-camp/webook/internal/domain"
 	"gorm.io/gorm"
+)
+
+var (
+	ErrUserDuplicateEmail = errors.New("邮箱冲突")
+	ErrUserNotFound       = gorm.ErrRecordNotFound
+	// ErrInvalidUserOrPassword = gorm.ErrRecordNotFound
 )
 
 type UserDao struct {
@@ -17,11 +26,32 @@ func NewUserDao(db *gorm.DB) *UserDao {
 	}
 }
 
+func (dao *UserDao) FindByEmail(ctx context.Context, email string) (User, error) {
+	var u User
+	err := dao.db.WithContext(ctx).Where("email=?", email).First(&u).Error
+	// err := dao.db.WithContext(ctx).First(&u, "email=?", email).Error
+	return u, err
+}
+
 func (dao *UserDao) Insert(ctx context.Context, u User) error {
 	now := time.Now().UnixMilli() // 毫秒数, 根据具体业务场景
 	u.Ctime = now
 	u.Utime = now
-	return dao.db.WithContext(ctx).Create(&u).Error
+
+	// 检查邮箱是否冲突, 与底层强耦合
+	err := dao.db.WithContext(ctx).Create(&u).Error
+	if mysqlErr, ok := err.(*mysql.MySQLError); ok { // 因为 email 字段设置了 unique 约束（唯一索引），所以会报错
+		if mysqlErr.Number == 1062 {
+			return ErrUserDuplicateEmail
+		}
+	}
+	return err
+	// return dao.db.WithContext(ctx).Create(&u).Error
+}
+
+func (dao *UserDao) Update(ctx context.Context, u domain.User) error {
+	// 更新 u 中的信息
+	return dao.db.WithContext(ctx).Save(&u).Error
 }
 
 // User 直接对应数据库 User 表结构， 还有其他叫法：entity， model，PO(persistent object)
--- go-basic-camp/webook/internal/service/user.go
+++ go-basic-camp/webook/internal/service/user.go
@@ -2,9 +2,18 @@ package service
 
 import (
 	"context"
+	"errors"
 
 	"github.com/upupnoah/go-basic-future/go-basic-camp/webook/internal/domain"
 	"github.com/upupnoah/go-basic-future/go-basic-camp/webook/internal/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrUserDuplicateEmail = repository.ErrUserDuplicateEmail
+	// ErrUserNotFound       = repository.ErrUserNotFound
+	ErrInvalidUserOrPassword = errors.New("账号/邮箱 或 密码错误")
+	ErrUserNotFound          = errors.New("用户不存在")
 )
 
 // UserService 用户服务
@@ -12,15 +21,44 @@ type UserService struct {
 	repo *repository.UserRepository
 }
 
-func NewUserService(repo *repository.UserRepository)  *UserService{
-	return &UserService {
+func NewUserService(repo *repository.UserRepository) *UserService {
+	return &UserService{
 		repo: repo,
 	}
 }
 
 // SignUp 注册， 不是 Create ，因为有可能是邀请注册的，那就需要使用另一套逻辑
 func (usvc *UserService) SignUp(ctx context.Context, u domain.User) error { // 不知道返回啥的时候就返回error
-	// todo 考虑加密放在哪里
+	// 密码加密
+	bPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(bPassword)
+
 	//  存储到数据库
 	return usvc.repo.Create(ctx, u)
+}
+
+func (usvc *UserService) Login(ctx context.Context, email, password string) (domain.User, error) {
+	// 先找用户
+	u, err := usvc.repo.FindByEmail(ctx, email)
+	if err == repository.ErrUserNotFound {
+		return domain.User{}, ErrUserNotFound
+	}
+	if err != nil {
+		return domain.User{}, err
+	}
+	// 比较密码
+	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil {
+		// DEBUG 日志，方便测试
+		// log.Printf("password compare failed, err: %v", err)
+		return domain.User{}, ErrInvalidUserOrPassword
+	}
+	return domain.User{}, nil
+}
+
+func (usvc *UserService) UpdateProfile(ctx context.Context, u domain.User) error {
+	return usvc.repo.Update(ctx, u)
 }
